49be0952438086b3c8b40bbbbcea54c62f7f0604
yagnadeepxo <77982741+yagnadeepxo@users.noreply.github.com>
2022-01-25 22:49:46 +0530

    error correction in merkle root

--- javaproject/MerkleRoot.java
+++ javaproject/MerkleRoot.java
@@ -6,7 +6,7 @@ public class MerkleRoot{
 		
 		String leaf1 = StringUtil.applySha512(txnlist.get(0).transactionId+txnlist.get(1).transactionId);
 		String d_leaf1 = StringUtil.applySha512(leaf1);
-		String leaf2 = StringUtil.applySha512(txnlist.get(0).transactionId+txnlist.get(1).transactionId);
+		String leaf2 = StringUtil.applySha512(txnlist.get(1).transactionId+txnlist.get(3).transactionId);
 		String d_leaf2 = StringUtil.applySha512(leaf2);
 		String merkle_root = StringUtil.applySha512(d_leaf1+d_leaf2);
 		return merkle_root;	
